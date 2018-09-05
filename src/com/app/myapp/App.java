1634814fa7665b0ce39a964929d7cb3db54ed85c
SANDEEP RAWAT <SANDEEP RAWAT@SANDEEP>
2018-09-05 14:46:12 +0530

    some new modification

--- src/com/app/myapp/App.java
+++ src/com/app/myapp/App.java
@@ -2,5 +2,28 @@ package com.app.myapp;
 
 public class App {
 
+	private float version;
+	private String versionName;
+	
+	public float getVersion() {
+		return version;
+	}
+	
+	public void setVersion(float version) {
+		this.version = version;
+	}
+	
+	public String getVersionName() {
+		return versionName;
+	}
+	
+	public void setVersionName(String versionName) {
+		this.versionName = versionName;
+	}
+	
+	@Override
+	public String toString() {
+		return "App [version=" + version + ", versionName=" + versionName + "]";
+	}
 	
 }
